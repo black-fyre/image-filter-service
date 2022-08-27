01cd433b64dab44e93c1d794b01a624b57d96268
Fehintoluwa Dahunsi <fehintoluwa@FehintoluwasMBP.lan>
2022-08-27 10:36:59 +0100

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  app.get( "/filteredimage", async ( req, res ) => {
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`Image url is required`);
+    }
+
+    filterImageFromURL(image_url).then((filteredImage) => {
+      res.status(200).sendFile(filteredImage, () =>{
+        deleteLocalFiles([filteredImage])
+      })
+    })
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
